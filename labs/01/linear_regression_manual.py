3fc5f7e1100fba1103f96abc1a345f816b1b3df9
Kobra_Mohamadi <kobra.mohammadi761@gmail.com>
2024-09-10 16:07:59 +0330

    Update linear_regression_manual.py

--- labs/01/linear_regression_manual.py
+++ labs/01/linear_regression_manual.py
@@ -18,25 +18,32 @@ def main(args: argparse.Namespace) -> float:
     dataset = sklearn.datasets.load_diabetes()
 
     # The input data are in `dataset.data`, targets are in `dataset.target`.
-
+    x=dataset.data
+    y=dataset.target
     # If you want to learn about the dataset, you can print some information
     # about it using `print(dataset.DESCR)`.
 
     # TODO: Append a constant feature with value 1 to the end of every input data.
+    x=np.c_[x,np.ones(x.shape[0])]
     # Then we do not need to explicitly represent bias - it becomes the last weight.
 
+    
     # TODO: Split the dataset into a train set and a test set.
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=args.test_size, random_state=args.seed)
     # Use `sklearn.model_selection.train_test_split` method call, passing
     # arguments `test_size=args.test_size, random_state=args.seed`.
 
     # TODO: Solve the linear regression using the algorithm from the lecture,
+    w = np.linalg.inv(x_train.T @ x_train) @ x_train.T @ y_train
     # explicitly computing the matrix inverse (using `np.linalg.inv`).
 
+    
     # TODO: Predict target values on the test set.
+    y_pred = x_test @ w
 
-    # TODO: Manually compute root mean square error on the test set predictions.
-    rmse = ...
-
+    
+    # TODO: Manually compute root mean square error on the test set predictions
+    rmse =np.sqrt(np.mean((y_test - y_pred) ** 2))
     return rmse
 
 
